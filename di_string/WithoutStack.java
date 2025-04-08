79bab8e3db68c8aa37c23e8f06c3485e2c5abd0c
Sorna Prabhakaran P <154865676+PSprabha@users.noreply.github.com>
2025-04-08 20:24:48 +0530

    Add files via upload

---
+++ di_string/WithoutStack.java
@@ -0,0 +1,33 @@
+package di_string;
+
+public class WithoutStack 
+{
+	static void diString()
+	{
+		String input = "IIIDIDDD";
+        String res = "";
+        int prev = 0;
+
+        for(int i = 0; i <= input.length(); i++)
+        {
+            if(i == input.length() || input.charAt(i) == 'I')
+            {
+                int crnt = i+1;
+
+                while(crnt > prev)
+                {
+                    res += (crnt--) + "";
+                }
+                prev = i+1;
+            }
+        }
+
+        System.out.println("without using stack : " + res);
+	}
+
+	public static void main(String[] args) 
+	{
+		diString();
+	}
+
+}
